e56ffb979ee9643e22130440c7e93c4af09d47dc
abdusharip <abdusharipa9@gmail.com>
2025-03-10 12:57:02 +0500

    added cors url for netlify

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(
 	cors({
-		origin: ['http://127.0.0.1:5500'],
+		origin: ['http://127.0.0.1:5500', 'https://a1kafe-mod.netlify.app/'],
 		credentials: true,
 	})
 )
